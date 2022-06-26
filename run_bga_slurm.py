52a1d26ef89fdb5aaa10fa561683a10fb3a68935
Stephen Maldonado <maldonado527@gmail.com>
2022-06-26 18:20:58 +0300

    s

--- run_bga_slurm.py
+++ run_bga_slurm.py
@@ -1,124 +1,48 @@
-import pygenalg
-from psuedologger import psuedologger
-import json, os
-import numpy as np
-import pandas as pd
-import sys
-from tqdm import tqdm
-
-incl_all_indvs = True
-incl_spc_indvs = True
-
-incl_all_track = False
-incl_turns = True
-
-incl_all_tracks = True
-incl_spc_tracks = True
-
-log = psuedologger(file_out='log.out', file_lvl=10)
-
-train_file = sys.argv[1]
-try:
-    gene_size = sys.argv[2]
-except:
-    gene_size = 16
+#!/bin/bash
+#SBATCH -o slurm_out.txt
+#SBATCH -e slurm_err.txt
+#SBATCH --time=120:00:00
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%J.out
 
 
-test_file = train_file.replace('training', 'test')
+export file="run_bga.py"
 
-split = train_file.split('-')
+mkdir $SLURM_JOB_ID
+export debug_logs="$SLURM_JOB_ID/job_$SLURM_JOB_ID.log"
+export benchmark_logs="$SLURM_JOB_ID/job_$SLURM_JOB_ID.log"
 
-dir_path = f'results/bga-{split[0]}-{split[1]}-{split[2]}/'
+module load python/python-2.7.14-ic-2017.1.043
+module load openmpi/openmpi-2.1.1-ic-2017.1.043
 
-if split[0] == 'all_indvs':
-    if not incl_all_indvs:
-        print('Exiting')
-        exit()
-elif not incl_spc_indvs:
-    print('Exiting')
-    exit()
+conda activate pga_adhd_runs
 
-if split[1] == 'all_tracks':
-    if not incl_all_tracks:
-        print('Exiting')
-        exit()
-elif not incl_spc_tracks:
-    print('Exiting')
-    exit()
+## Enter Working Directory ##
+cd $SLURM_SUBMIT_DIR
+## Create Log File ##
+echo $SLURM_SUBMIT_DIR
+echo "JobID: $SLURM_JOB_ID" >> $debug_logs
+echo "Running on $SLURM_NODELIST" >> $debug_logs
+echo "Running on $SLURM_NNODES nodes." >> $debug_logs
+echo "Running on $SLURM_NPROCS processors." >> $debug_logs
+echo  "Current working directory is `pwd`" >> $debug_logs
 
-if split[2] == 'all_track':
-    if not incl_all_track:
-        print('Exiting')
-        exit()
-elif not incl_turns:
-    print('Exiting')
-    exit()
+## Module debugging ##
+module list >> $debug_logs
+which mpirun >> $debug_logs
 
-# Skip if we already got this directory results
-dir_path = f'results_new/bga-{split[0]}-{split[1]}-{split[2]}/'
+date >> $benchmark_logs
+echo "ulimit -l: " >> $benchmark_logs
+ulimit -l >> $benchmark_logs
 
-if os.path.isdir(dir_path):
-    if 'config.json' in os.listdir(dir_path):
-        print('Exiting')
-        exit()
-    if os.path.exists(os.path.join(dir_path, 'populations')) and \
-                len(os.listdir(os.path.join(dir_path, 'populations'))) == 100:
-        print('Exiting')
-        exit()
+## Run job ##
+mpirun -np $SLURM_NTASKS python $file $args
+sleep 3
 
+date >> $benchmark_logs
+echo "ulimit -l" >> $benchmark_logs
+ulimit -l >> $benchmark_logs
 
-try:
-    # Get the training file
-    train = os.path.join('datasets/driving/',train_file)
-
-    # Get the test file
-    test_file_n = '-'.join(split[:-1])+'-test.csv'
-    test = os.path.join('datasets/driving/',test_file)
-
-    # Read in the training data
-    feats = pd.read_csv(train)
-    feats.drop(columns=feats.columns[0], axis=1, inplace=True)
-    feats.drop(columns=feats.columns[0], axis=1, inplace=True)
-    lbls = feats.pop('Medicated')
-
-    # Read in the testing data
-    test_feats = pd.read_csv(test)
-    test_feats.drop(columns=test_feats.columns[0], axis=1, inplace=True)
-    test_feats.drop(columns=test_feats.columns[0], axis=1, inplace=True)
-    test_lbls = test_feats.pop('Medicated')
-
-    # Parameters
-    cfg = {'evaluator':'logisticRegressionEvaluator',\
-           'rep':'binary',
-           'n_genes':59,\
-           'gene_size':gene_size,\
-           'length':59,\
-           'min':-10.0,\
-           'max':10.0,\
-           'dtype':float,\
-           'log':log,\
-           'n_runs':50,\
-           'n_gens':200,\
-           'train_feats':feats,\
-           'train_lbls':lbls, \
-           'test_feats':test_feats,\
-           'test_lbls':test_lbls,\
-           'store_each_gen':True,\
-           'standardize':True,\
-           'has_constant':True,\
-           'dir_path':dir_path,\
-           'disable_tqdm':True
-          }
-
-    ga = pygenalg.geneticAlgorithm(config=cfg)
-    x = ga.run()
-except KeyboardInterrupt:
-    print('Exiting')
-    exit()
-except Exception as e:
-    try:
-        tqdm.write(f'Failed {split[0]}, {split[1]}, {split[2]}')
-    except:
-        tqdm.write('failed')
-    tqdm.write(str(e))
-    exit()
+## Directory Cleanup ##
+mv job.$SLURM_JOB_ID.err $SLURM_JOB_ID/
+mv job.$SLURM_JOB_ID.out $SLURM_JOB_ID/
